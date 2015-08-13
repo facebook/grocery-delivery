543c3c71fbba3cd9c7dd2014961e6575e31fe263
odcinek <odcinek@ix.nest>
2015-08-12 19:18:10 -0700

    Test svn support

---
+++ test/run-git.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+set -e
+set -o verbose
+bundle exec chef-zero &
+ZERO=$!
+SOURCE=/tmp/source
+sleep 5
+
+rm -rf /tmp/master_path
+rm -rf $SOURCE
+GIT="git -c user.email='foo@bar.com' -c user.name='foobar'"
+
+function gd {
+  bundle exec ./bin/grocery-delivery -c test/gd-git.rb -vv
+}
+
+(
+  mkdir $SOURCE
+  $GIT init $SOURCE
+  cd $SOURCE && touch test && $GIT add . -A && $GIT commit -m "Dummy commit"
+)
+gd
+(
+  cd $SOURCE
+  mkdir chef/cookbooks/cookbook1/recipes -p
+  touch chef/cookbooks/cookbook1/recipes/default.rb
+  echo "name 'cookbook1'" > chef/cookbooks/cookbook1/metadata.rb
+  $GIT add . -A
+  $GIT commit -m "Add cookbook1"
+)
+gd
+(
+  cd $SOURCE
+  touch chef/cookbooks/cookbook1/recipes/recipe2.rb
+  $GIT add . -A
+  $GIT commit -m "Modify cookbook1"
+)
+gd
+(
+  cd $SOURCE
+  rm -rf chef/cookbooks/cookbook1
+  $GIT add . -A
+  $GIT commit -m "Remove cookbook1"
+)
+gd
+
+kill -9 $ZERO
---
+++ test/run-svn.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+set -e
+set -o verbose
+bundle exec chef-zero &
+ZERO=$!
+SOURCE=/tmp/source
+sleep 5
+
+rm -rf /tmp/master_path
+rm -rf $SOURCE
+rm -rf $SOURCE-import
+rm -rf $SOURCE-checkout
+
+function gd {
+  bundle exec ./bin/grocery-delivery -c test/gd-svn.rb -vv
+}
+
+(
+  mkdir $SOURCE
+  svnadmin create $SOURCE
+  mkdir $SOURCE-import && cd $SOURCE-import && touch test && svn import . file://$SOURCE -m"Dummy commit"
+)
+gd
+(
+  svn checkout file://$SOURCE $SOURCE-checkout
+  cd $SOURCE-checkout
+  mkdir chef/cookbooks/cookbook1/recipes -p
+  touch chef/cookbooks/cookbook1/recipes/default.rb
+  echo "name 'cookbook1'" > chef/cookbooks/cookbook1/metadata.rb
+  svn add chef
+  svn commit chef -m"Add cookbook1"
+)
+gd
+(
+  cd $SOURCE-checkout
+  touch chef/cookbooks/cookbook1/recipes/recipe2.rb
+  svn add chef/cookbooks/cookbook1/recipes/recipe2.rb
+  svn commit chef -m"Modify cookbook1"
+)
+gd
+(
+  cd $SOURCE-checkout
+  svn delete chef/cookbooks/cookbook1/metadata.rb
+  svn delete chef/cookbooks/cookbook1/recipes/default.rb
+  svn delete chef/cookbooks/cookbook1/recipes/recipe2.rb
+  svn status
+  svn commit -m"Remove cookbook1"
+)
+gd
+
+kill -9 $ZERO
